56076e0dc54f17e654e245680575cf0c55d7719c
dusvyat <13661123+dusvyat@users.noreply.github.com>
2024-02-12 20:03:41 +0200

    Refactor AutoRetrieval class and create SQLRetriever
    Renamed 'auto_retrieval.py' to 'retrievers.py' and enhanced its functionality. Refactored class AutoRetrieval into AutoRetriever, and added enhancements. Included a new class SQLRetriever which connects to Postgres DB and allows SQL retrieval. This commit also includes numerous changes related to imports, prompt templates, and methods of these classes.

--- rag/retrieval/retrievers.py
+++ rag/retrieval/retrievers.py
@@ -1,18 +1,32 @@
 from typing import List
 
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.vectorstores import VectorStore
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain.vectorstores import Chroma
+from langchain.sql_database import SQLDatabase
+from langchain_community.vectorstores import Chroma
 
 from langchain.retrievers.self_query.base import SelfQueryRetriever
 from langchain.docstore.document import Document
 
-from settings import DEFAULT_LLM_MODEL, DEFAULT_PROMPT_TEMPLATE
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+import re
+from langchain_core.runnables import RunnableLambda
+
+from rag.settings import (DEFAULT_LLM_MODEL,
+                          DEFAULT_AUTO_META_PROMPT_TEMPLATE,
+                          DEFAULT_SQL_RESULT_PROMPT,
+                          DEFAULT_TEXT_2_PGVECTOR_PROMPT
+                          )
 import pandas as pd
 import json
 
 
-class AutoRetrieval:
+# to do make base class
+
+
+class AutoRetriever:
     """
     AutoRetrieval is a class that uses langchain to extract metadata from a dataframe and query it using self retrieval.
 
@@ -28,7 +42,7 @@ class AutoRetrieval:
             filter_columns: List[str] = None,
             document_description: str = "",
             model: str = DEFAULT_LLM_MODEL,
-            prompt_template: str = DEFAULT_PROMPT_TEMPLATE,
+            prompt_template: str = DEFAULT_AUTO_META_PROMPT_TEMPLATE,
             cardinality_threshold: int = 40
     ):
         """
@@ -50,6 +64,7 @@ class AutoRetrieval:
         self.filter_columns = filter_columns
         self.document_description = document_description
         self.llm = ChatOpenAI(model=model)
+        self.embeddings_model = OpenAIEmbeddings()
         self.prompt_template = prompt_template
         self.cardinality_threshold = cardinality_threshold
 
@@ -107,8 +122,25 @@ class AutoRetrieval:
         Given a dataframe, convert it to a list of documents and use it to create a vectorstore.
         :return:
         """
-        embedder = OpenAIEmbeddings()
-        self.vectorstore = Chroma.from_documents(self.df_to_documents(), embedder)
+        self.vectorstore = Chroma.from_documents(self.df_to_documents(), self.embeddings_model)
+
+    def get_retriever(self):
+        """
+        return the self-query retriever
+        :return:
+        """
+        if self.vectorstore is None:
+            # if no vector stor is passed on init, create one from input data.
+
+            self.get_vectorstore()
+
+        return SelfQueryRetriever.from_llm(
+            llm=self.llm,
+            vectorstore=self.vectorstore,
+            document_contents=self.document_description,
+            metadata_field_info=self.get_metadata_field_info(),
+            verbose=True
+        )
 
     def query(self, question: str) -> List[Document]:
         """
@@ -116,22 +148,77 @@ class AutoRetrieval:
         :param question: str
         :return: List[Document]
         """
+        retriever = self.get_retriever()
 
-        if self.vectorstore is None:
-            self.get_vectorstore()
-
-        retriever = SelfQueryRetriever.from_llm(
-            llm=self.llm,
-            vectorstore=self.vectorstore,
-            document_contents=self.document_description,
-            metadata_field_info=self.get_metadata_field_info(),
-            verbose=True
-        )
         return retriever.get_relevant_documents(question)
 
 
-if __name__ == "__main__":
-    df = pd.read_csv("./data/test/movie_test.csv")
-    df.fillna("unknown", inplace=True)
-    ar = AutoRetrieval(df, "content", document_description="Brief summary of a movie")
-    print(ar.query("I want to watch a movie rated higher than 8.5"))
+class SQLRetriever:
+    """
+    a retriever used to connect to a postgres DB with pgvector extension
+    """
+
+    def __init__(self,
+                 user: str,
+                 host: str,
+                 port: int,
+                 db_name: str,
+                 sql_query_prompt_template: str = DEFAULT_TEXT_2_PGVECTOR_PROMPT,
+                 sql_result_prompt_template: str = DEFAULT_SQL_RESULT_PROMPT
+                 ):
+        connection_string = f"postgresql+psycopg2://postgres:{user}@{host}:{port}/{db_name}"
+        self.db = SQLDatabase.from_uri(connection_string)
+        self.llm = ChatOpenAI(model_name="gpt-4", temperature=0)
+        self.sql_query_prompt_template = sql_query_prompt_template
+        self.sql_result_prompt_template = sql_result_prompt_template
+        self.embeddings_model = OpenAIEmbeddings()
+
+    @staticmethod
+    def format_prompt(prompt_template: str):
+        """
+        format prompt template
+
+        :return:
+        """
+        return ChatPromptTemplate.from_messages(
+            [("system", prompt_template), ("human", "{question}")]
+        )
+
+    def get_schema(self):
+        """
+        Get DB schema
+        :return:
+        """
+        return self.db.get_table_info()
+
+    def replace_brackets(self, match):
+        words_inside_brackets = match.group(1).split(", ")
+        embedded_words = [
+            str(self.embeddings_model.embed_query(word)) for word in words_inside_brackets
+        ]
+        return "', '".join(embedded_words)
+
+    def get_query(self, query):
+        sql_query = re.sub(r"\[([\w\s,]+)\]", self.replace_brackets, query)
+        return sql_query
+
+    @property
+    def sql_query_chain(self):
+        return (
+                RunnablePassthrough.assign(schema=self.get_schema)
+                | self.format_prompt(self.sql_query_prompt_template)
+                | self.llm.bind(stop=["\nSQLResult:"])
+                | StrOutputParser()
+        )
+
+    @property
+    def full_chain(self):
+        return (
+                RunnablePassthrough.assign(query=self.sql_query_chain)
+                | RunnablePassthrough.assign(
+            schema=self.get_schema,
+            response=RunnableLambda(lambda x: self.db.run(self.get_query(x["query"]))),
+        )
+                | self.format_prompt(self.sql_result_prompt_template)
+                | self.llm
+        )
