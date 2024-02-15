9e83cc9e8e58a3072940dedb8c350b3b11c35904
tmichaeldb <tyler@mindsdb.com>
2024-02-14 16:39:48 -0800

    Add simple email RAG evaluation pipeline

--- evaluate_email_rag.py
+++ evaluate_email_rag.py
@@ -11,10 +11,12 @@ from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 
 from evaluate import evaluate
-from ingestors.file_ingestor import FileIngestor
+from ingestors.email_ingestor.email_client import EmailClient
+from ingestors.email_ingestor.email_ingestor import EmailIngestor
+from ingestors.email_ingestor.email_search_options import EmailSearchOptions
 
 
-# Use a basic QA prompt template for file RAG evaluation.
+# Use a basic QA prompt template for email RAG evaluation.
 _EVALUATION_PROMPT_TEMPLATE = '''You are an assistant for
 question-answering tasks. Use the following pieces of retrieved context
 to answer the question. If you don't know the answer, just say that you
@@ -24,26 +26,32 @@ Context: {context}
 Answer:'''
 
 
-def evaluate_file_rag(dataset: str):
+def evaluate_email_rag(dataset: str):
     '''Evaluates a RAG pipeline that answers questions from a dataset
-    about various types of files, depending on the dataset.
+    about various emails, depending on the dataset.
     '''
-    source_files_path = os.path.join('./data', dataset, 'source_files')
-    source_files = []
-    for f in os.listdir(path=source_files_path):
-        full_path = os.path.join(source_files_path, f)
-        if not os.path.isfile(full_path):
-            continue
-        source_files.append(full_path)
+    username = os.getenv('EMAIL_USERNAME')
+    password = os.getenv('EMAIL_PASSWORD')
+    email_client = EmailClient(username, password)
 
-    ingestor = FileIngestor(source_files)
-    logging.info('Loading documents from {}'.format(source_files_path))
-    documents = ingestor.ingest()
-    logging.info('Documents loaded')
+    # Use default search options to get emails from the last 10 days.
+    search_options = EmailSearchOptions(
+        mailbox='INBOX',
+        subject=None,
+        to_email=None,
+        from_email=None,
+        since_date=None,
+        until_date=None,
+        since_email_id=None
+    )
+    email_ingestor = EmailIngestor(email_client, search_options)
+    logging.info('Ingesting emails')
+    all_documents = email_ingestor.ingest()
+    logging.info('Ingested')
 
     # For now, use Chroma for evaluation.
     vectorstore = Chroma.from_documents(
-        documents=documents,
+        documents=all_documents,
         embedding=OpenAIEmbeddings(),
     )
     retriever = vectorstore.as_retriever()
@@ -53,7 +61,7 @@ def evaluate_file_rag(dataset: str):
     llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0)
     prompt = ChatPromptTemplate.from_template(_EVALUATION_PROMPT_TEMPLATE)
 
-    # Setup RAG pipeline
+    # Setup simple RAG pipeline
     rag_chain = (
         {'context': retriever,  'question': RunnablePassthrough()}
         | prompt
@@ -63,7 +71,7 @@ def evaluate_file_rag(dataset: str):
 
     # Generate filename based on current datetime.
     dt_string = datetime.now().strftime('%d%m%Y_%H%M%S')
-    output_file = 'evaluate_file_rag_{}.csv'.format(dt_string)
+    output_file = 'evaluate_email_rag_{}.csv'.format(dt_string)
 
     qa_file = os.path.join('./data', dataset, 'rag_dataset.json')
 
@@ -79,15 +87,18 @@ if __name__ == '__main__':
         asyncio.set_event_loop_policy(
             asyncio.WindowsSelectorEventLoopPolicy())
 
-    # To use, set the environment variableOPENAI_API_KEY='<YOUR_API_KEY>'
+    # To use, set the environment variables:
+    # OPENAI_API_KEY='<YOUR_API_KEY>'
+    # EMAIL_USERNAME='<YOUR_EMAIL_USERNAME>'
+    # EMAIL_PASSWORD='<YOUR_PASSWORD>'
     parser = argparse.ArgumentParser(
-        prog='Evaluate File RAG',
-        description='''Evaluates the performance of a RAG pipeline with files.
+        prog='Evaluate Email RAG',
+        description='''Evaluates the performance of a RAG pipeline with email.
 Uses evaluation metrics from the RAGAs library.
         '''
     )
     parser.add_argument(
-        '-d', '--dataset', help='Name of QA dataset to use for evaluation (e.g. blockchain_solana)')
+        '-d', '--dataset', help='Name of QA dataset to use for evaluation (e.g. personal_emails)')
     parser.add_argument(
         '-l', '--log', help='Logging level to use (default WARNING)', default='WARNING')
 
@@ -95,4 +106,4 @@ Uses evaluation metrics from the RAGAs library.
     log_level = getattr(logging, args.log.upper())
     logging.basicConfig(level=log_level)
 
-    evaluate_file_rag(args.dataset)
+    evaluate_email_rag(args.dataset)
