deaa0b506378f5f45ec82d41d6b9d5884b9b5c5e
Daniel Usvyat <usvyat@gmail.com>
2024-03-26 12:19:28 +0200

    RAG-46 Enable loading of existing pgvector table created in mindsdb to langchain pgvector client (#28)
    * Implement data loading for vector stores
    
    The VectorStoreFactory class has been refactored to import custom configurations for Chroma and PGVector stores for enhanced modularity. Additional utility functions support the extraction of data from databases and its insertion into the vector stores, thus enabling dynamic data loading.
    
    * adding docs

--- loaders/vector_store_loader/vector_store_loader.py
+++ loaders/vector_store_loader/vector_store_loader.py
@@ -1,8 +1,21 @@
+import ast
+import uuid
+
 from langchain_core.embeddings import Embeddings
 from langchain_community.vectorstores import Chroma, PGVector
 from langchain_core.vectorstores import VectorStore
 from pydantic import BaseModel, parse_obj_as
 from settings import VectorStoreType, VectorStoreConfig
+import pandas as pd
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.exc import DisconnectionError
+
+_COL_ID = "id"
+_COL_EMBEDDINGS = "embeddings"
+_COL_METADATA = "metadata"
+_COL_CONTENT = "content"
 
 
 class VectorStoreFactory:
@@ -13,14 +26,14 @@ class VectorStoreFactory:
         else:
             settings = VectorStoreConfig()
         if settings.type == VectorStoreType.CHROMA:
-            return VectorStoreFactory._create_chroma_vectorstore(embeddings_model, settings)
+            return VectorStoreFactory._load_chromadb_store(embeddings_model, settings)
         elif settings.type == VectorStoreType.PGVECTOR:
-            return VectorStoreFactory._create_pgvector_vectorstore(embeddings_model, settings)
+            return VectorStoreFactory._load_pgvector_store(embeddings_model, settings)
         else:
             raise ValueError(f"Invalid vector store type, must be one either {VectorStoreType.__members__.keys()}")
 
     @staticmethod
-    def _create_chroma_vectorstore(embeddings_model: Embeddings, settings):
+    def _load_chromadb_store(embeddings_model: Embeddings, settings) -> Chroma:
         return Chroma(
             persist_directory=settings.persist_directory,
             collection_name=settings.collection_name,
@@ -28,11 +41,61 @@ class VectorStoreFactory:
         )
 
     @staticmethod
-    def _create_pgvector_vectorstore(embeddings_model: Embeddings, settings):
-        return PGVector.from_existing_index(
-            embedding=embeddings_model,
-            **settings.dict()
+    def _load_pgvector_store(embeddings_model: Embeddings, settings) -> PGVector:
+        # create an empty store if collection_name does not exist otherwise load the existing collection
+        store = PGVector(
+            connection_string=settings.connection_string,
+            collection_name=settings.collection_name,
+            embedding_function=embeddings_model
         )
+        return VectorStoreFactory._load_data_into_langchain_pgvector(settings, store)
+
+    @staticmethod
+    def _load_data_into_langchain_pgvector(settings, vectorstore: PGVector) -> PGVector:
+        """
+        Fetches data from the existing pgvector table and loads it into the langchain pgvector vector store
+        :param settings:
+        :param vectorstore:
+        :return:
+        """
+        df = VectorStoreFactory._fetch_data_from_db(settings)
+
+        df[_COL_EMBEDDINGS] = df[_COL_EMBEDDINGS].apply(ast.literal_eval)
+        df[_COL_METADATA] = df[_COL_METADATA].apply(ast.literal_eval)
+
+        metadata = df[_COL_METADATA].tolist()
+        embeddings = df[_COL_EMBEDDINGS].tolist()
+        texts = df[_COL_CONTENT].tolist()
+        ids = [str(uuid.uuid1()) for _ in range(len(df))] if _COL_ID not in df.columns else df[_COL_ID].tolist()
+
+        vectorstore.add_embeddings(
+            texts=texts,
+            embeddings=embeddings,
+            metadatas=metadata,
+            ids=ids
+        )
+        return vectorstore
+
+    @staticmethod
+    def _fetch_data_from_db(settings: VectorStoreConfig) -> pd.DataFrame:
+        """
+        Fetches data from the database using the provided connection_string in the settings
+        :param settings:
+        :return:
+        """
+        try:
+            engine = create_engine(settings.connection_string)
+            db = scoped_session(sessionmaker(bind=engine))
+
+            df = pd.read_sql(f"SELECT * FROM {settings.collection_name}", engine)
+
+            return df
+        except DisconnectionError as e:
+            # todo replace with logger when integrated in mindsdb
+            print("Unable to connect to the database. Please check your connection string and try again.")
+            raise e
+        finally:
+            db.close()
 
 
 class VectorStoreLoader(BaseModel):
@@ -46,10 +109,20 @@ class VectorStoreLoader(BaseModel):
         validate_assignment = True
 
     def load(self) -> VectorStore:
+        """
+        Loads the vector store based on the provided config and embeddings model
+        :return:
+        """
         self.vector_store = VectorStoreFactory.create(self.embeddings_model, self.config)
         return self.vector_store
 
 
 def load_vector_store(embeddings_model: Embeddings, config: dict={}) -> VectorStore:
+    """
+    Loads the vector store based on the provided config and embeddings model
+    :param embeddings_model:
+    :param config:
+    :return:
+    """
     loader = VectorStoreLoader(embeddings_model=embeddings_model, config=config)
     return loader.load()
